625b641211230316c9bb814d93b8d556ad06a430
Ceneve <boriskozlow@gmail.com>
2024-09-29 18:30:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) {
+        n = n * -1;
+    }
+  let str = String(n);
+  let newStr = '';
+  for(let i = str.length - 1; i >= 0 ; i--){
+    newStr += str[i];
+  }
+  return Number(newStr);
 }
